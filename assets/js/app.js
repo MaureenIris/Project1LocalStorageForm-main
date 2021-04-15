f230b96979728e94fb2bbfe918da54334f393776
MaureenIris <bonifaciomaureeniris@gmail.com>
2021-04-15 11:24:33 +0800

    Adding new elements into DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //Variables
-
+const tweetList = document.getElementById('tweet-list');
 
 //EventListeners
 eventListeners();
@@ -15,5 +15,23 @@ function eventListeners(){
 function newTweet(e){ 
     e.preventDefault();
 
-    console.log('Form Submitted');
+    //Read the textarea value
+    const tweet = document.getElementById('tweet').value;
+
+    //Create the remove Button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
+
+    //Createan <li> element
+    const li = document.createElement('li');
+    li.textContent = tweet;
+    tweetList.appendChild(li);
+
+    //Add and remove button to each tweet
+    li.appendChild(removeBtn);
+
+    //Add to the list
+    tweetList.appendChild(li);
 }
