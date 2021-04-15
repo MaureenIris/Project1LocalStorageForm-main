383b35296a4074cbdae01a8cd38983c434d66ab2
MaureenIris <bonifaciomaureeniris@gmail.com>
2021-04-15 12:28:02 +0800

    Removing Elements from the Storage part 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,6 +49,9 @@ function removeTweet(e){
     if(e.target.classList.contains('remove-tweet')){
         e.target.parentElement.remove();
     }
+    
+    //Remove from Storage
+    removeTweetLocalStorage(e.target.parentElement.textContent);
 }
 
 //Adds the tweets into the local storage
@@ -96,5 +99,22 @@ function localStorageOnLoad() {
 
         //Add to the list
         tweetList.appendChild(li);
+    });
+}
+
+//Removes the tweet from local storage
+function removeTweetLocalStorage(tweet){
+    // get tweets from storage
+    let tweets = getTweetsFromStorage();
+
+    //remove the X from the tweet
+
+    const tweetDelete = tweet.substring(0, tweet.length -1);
+
+    //Loop through the tweets and remove the tweet that's equal
+    tweets.forEach(function(tweetLS){
+        if(tweetDelete === tweetLS){
+            console.log('Yes');
+        }
     });
 }
