40cefa9af0c1ddd84da8c748d394325a4a9ab5e8
MaureenIris <bonifaciomaureeniris@gmail.com>
2021-04-15 12:15:38 +0800

    Print Elements from Storage on Load

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,8 @@ function eventListeners(){
     //Remove tweet from the list
     tweetList.addEventListener('click', removeTweet);
 
+    //Document
+    document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 }
 
 //Functions
@@ -70,4 +72,29 @@ function getTweetsFromStorage() {
         tweets = JSON.parse ( tweetsLS );
     }
     return tweets;
+}
+
+//Prints Local Storage Tweets on Load
+function localStorageOnLoad() {
+    let tweets = getTweetsFromStorage();
+
+    // Loop through storage and then print the values
+    tweets.forEach(function(tweet){
+        //Create the remove Button
+        const removeBtn = document.createElement('a');
+        removeBtn.classList = 'remove-tweet';
+        removeBtn.textContent = 'X';
+
+
+        //Createan <li> element
+        const li = document.createElement('li');
+        li.textContent = tweet;
+        tweetList.appendChild(li);
+
+        //Add and remove button to each tweet
+        li.appendChild(removeBtn);
+
+        //Add to the list
+        tweetList.appendChild(li);
+    });
 }
