15d72c22b9edc77fc96f53977061aff8ef01075e
MaureenIris <bonifaciomaureeniris@gmail.com>
2021-04-15 10:41:47 +0800

    First Steps with the Project

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+//Variables
+
+
+//EventListeners
+eventListeners();
+
+function eventListeners(){
+    //Form Submission
+    document.querySelector('#form').addEventListener('submit', newTweet);
+
+}
+
+//Functions
+
+function newTweet(e){ 
+    e.preventDefault();
+
+    console.log('Form Submitted');
+}
